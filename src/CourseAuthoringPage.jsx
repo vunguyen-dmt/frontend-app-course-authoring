9eff0f080fa067e52334732fc3ced6a4f0a19caa
Vu Nguyen <vu.nguyen@dmttek.com>
2023-12-16 13:34:21 +0700

    update footer

--- src/CourseAuthoringPage.jsx
+++ src/CourseAuthoringPage.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   useLocation,
 } from 'react-router-dom';
-import { Footer } from '@edx/frontend-component-footer';
+import Footer from '@edx/frontend-component-footer';
 import Header from './header';
 import { fetchCourseDetail } from './data/thunks';
 import { useModel } from './generic/model-store';
